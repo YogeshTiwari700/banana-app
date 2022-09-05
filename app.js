4304ca507e1620045eb38eaec340ee07c76ece5f
Yogesh Tiwari <106140073+YogeshTiwari700@users.noreply.github.com>
2022-09-05 17:22:45 +0530

    complete

--- app.js
+++ app.js
@@ -2,9 +2,28 @@ var btnTranslate=document.querySelector("#btn-translate");
 var textInput=document.querySelector("#text-input")
 var outPut=document.querySelector("#output")
 
-console.log(outPut)
+// var bananaServerUrl = "https://api.funtranslations.com/translate/emoji.json"
+	
+
+var bananaServerUrl = "https://api.funtranslations.com/translate/minion.json"
+	 function getTranslateURL (input) {
+    return bananaServerUrl + "?" + "text=" + input
+   }
+
+   function errorHandler (error) {
+    console.log("error occured", error);
+    alert ("something is going wrong with server ! try again some time")
+   }
+
 function clickHandler() {
-  outPut.innerText = "OIHCDIUCIV " + textInput.value;
+ var inputText = textInput.value;
+  fetch(getTranslateURL(inputText))
+  .then(response => response.json())
+  .then(json => {
+    var translatedText = json.contents.translated;
+    outPut.innerText = translatedText;
+  })
+  .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
